09c62a77617ff24257543825d473554257f21aa0
Shaun Stone <shauns@nutmeg.com>
2020-12-28 13:27:50 +0000

    add levels and world mapping

--- src/createLevels.js
+++ src/createLevels.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const fs = require('fs');
 
-const FILE_TO_COPY = path.resolve('Levels', 'BaseLevel.tscn');
+const FILE_TO_COPY = path.resolve('Levels', 'Level.tscn');
 
 const MAX_X_ROOM = 7;
 const MAX_Y_ROOM = 20;
+
+let id = 8
 
 for (y = -MAX_X_ROOM; y<= MAX_X_ROOM; y++) {
   for (x = 0; x <= MAX_Y_ROOM; x++) {
@@ -20,7 +22,8 @@ for (y = -MAX_X_ROOM; y<= MAX_X_ROOM; y++) {
         if (err) {
           return console.log(err);
         }
-        console.log('written', fileName);
+        console.log(`[ext_resource path="res://src/${fileName}" type="PackedScene" id=${id}]`)
+        id++;
       });
     });
   }
